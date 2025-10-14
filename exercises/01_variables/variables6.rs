950c92f7dcf968f2221784450025d69ac3802081
arthuralvesdl <arthuralveslacerda@gmail.com>
2025-10-14 11:50:33 -0300

    exercise resolution 1

--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -1,5 +1,5 @@
 // TODO: Change the line below to fix the compiler error.
-const NUMBER = 3;
+const NUMBER: u32 = 3; // Número inteiro sem sinal de 32 bits
 
 fn main() {
     println!("Number: {NUMBER}");
